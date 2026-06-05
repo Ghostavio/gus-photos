195fc3925a38a4aa808c63e4daa0b8e6275e2828
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2026-06-05 03:06:54 -0300

    feat(build): tier path helpers

---
+++ build/lib/paths.test.js
@@ -0,0 +1,15 @@
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+import { tierPath, fullUrl } from './paths.js';
+
+test('tierPath builds album-scoped tier paths', () => {
+  assert.equal(tierPath('acervo-01', 'IMG_2223', 'thumb'), 'img/acervo-01/IMG_2223.thumb.avif');
+  assert.equal(tierPath('acervo-01', 'IMG_2223', 'view'),  'img/acervo-01/IMG_2223.view.avif');
+});
+
+test('fullUrl points at the Release', () => {
+  assert.equal(
+    fullUrl('https://github.com/Ghostavio/gus-photos/releases/download/acervo-01', 'IMG_2223'),
+    'https://github.com/Ghostavio/gus-photos/releases/download/acervo-01/IMG_2223.full.avif'
+  );
+});
